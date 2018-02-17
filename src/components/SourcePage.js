3636548ef71ad3e33bf2f035e85b619d10c02aa7
Steven Peniche <stevenmpeniche@gmail.com>
2018-02-17 14:46:08 -0500

    Add 'CSS grid/Flex Box' & Style 'Search Bar'

--- src/components/SourcePage.js
+++ src/components/SourcePage.js
@@ -4,11 +4,21 @@ import './SourcePage.css';
 const SourcePage = (props) => {
   return (
     <div className="source-page">
-      <div className="source-title">
-        Hello {props.source.title}!
+      <div className="source-page-sidebar">
+        <div className="source-icon-container">
+          <img className="source-icon" src={props.source.faviconURL} alt={props.source.title} />
+        </div>
       </div>
-      <div>
-        <img src={props.source.faviconURL} alt={props.source.title} />
+      <div className="source-page-main">
+        <div className="source-page-search-bar">
+          <span className="source-page-search-bar-icon">
+            <i className="fas fa-search"></i>
+          </span>
+          <input className="source-page-search-input" type="text" placeholder="Search..."/>
+        </div>
+        <div className="source-title">
+          Hello {props.source.title}!
+        </div>
       </div>
     </div>
   );
