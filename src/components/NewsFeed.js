8a6cc4deffd46addf59ce2353b3e729e1656bf0f
Steven Peniche <stevenmpeniche@gmail.com>
2018-02-20 18:13:31 -0500

    Edit 'News Feed' Styling

--- src/components/NewsFeed.js
+++ src/components/NewsFeed.js
@@ -2,29 +2,37 @@ import React from 'react';
 import './NewsFeed.css';
 
 const NewsFeed = (props) => {
-  let hasImage = false;
+  let array = [{hasImage: false}, {hasImage: true}, {hasImage: false}, {hasImage: true}];
 
   return (
     <div className="news-feed-container">
-      <div className="news-feed-card">
-        {
-          hasImage ?
-          <div className="news-feed-card-img-container">
-            <img
-              className="news-feed-card-img"
-              src="http://via.placeholder.com/320x240"
-              alt="news"
-            />
-          </div>
-          :
-          <div></div>
-        }
-        <div className="news-feed-card-title">
-          My YC app: Dropbox - Throw awaydsfdsffdsf your USB drive
-        </div>
-        <div className="news-feed-card-author">
-          - Steven Peniche
-        </div>
+      <div className="news-feed-cards-container">
+      {
+        array.map((article, i) => {
+          return (
+            <div className="news-feed-card" key={i}>
+              {
+                article.hasImage ?
+                <div className="news-feed-card-img-container">
+                  <img
+                    className="news-feed-card-img"
+                    src="http://via.placeholder.com/280x200"
+                    alt="news"
+                  />
+                </div>
+                :
+                <div></div>
+              }
+              <div className="news-feed-card-title">
+                My YC app: Dropbox - Throw awaydsfdsffdsf your USB drive
+              </div>
+              <div className="news-feed-card-author">
+                - Steven Peniche
+              </div>
+            </div>
+          )
+        })
+      }
       </div>
     </div>
   );
