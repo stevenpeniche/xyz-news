f91541f59ad98277999758857fd1196c82109e97
Steven Peniche <stevenmpeniche@gmail.com>
2018-02-21 10:44:30 -0500

    Restructure App and Initialize Express API

---
+++ client/src/containers/App.js
@@ -0,0 +1,139 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import { BrowserRouter, Route } from 'react-router-dom';
+import NavBar from '../components/NavBar';
+import HomeContainer from './HomeContainer';
+import SourcePageContainer from './SourcePageContainer';
+import './App.css';
+
+const faviconFinder = (rootURL) => {
+  return `https://besticon-demo.herokuapp.com/icon?url=${rootURL}/&size=70..120..200`
+}
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.changeCurrentSource = this.changeCurrentSource.bind(this);
+    this.state = {
+      response: '',
+      sources: [
+        {
+          id: 'hacker-news',
+          title: 'Hacker News',
+          faviconURL: faviconFinder('https://news.ycombinator.com'),
+          primaryColor: '#ff6600',
+          topics: [
+            {
+              name: 'Top',
+              data: []
+            },
+            {
+              name: 'New',
+              data: []
+            },
+            {
+              name: 'Ask',
+              data: []
+            },
+            {
+              name: 'Jobs',
+              data: []
+            }
+          ]
+        },
+        {
+          id: 'bleacher-report',
+          title: 'Bleacher Report',
+          faviconURL: faviconFinder('https://bleacherreport.com'),
+          primaryColor: '#000000',
+          topics: [
+            {
+              name: 'MLB',
+              data: []
+            },
+            {
+              name: 'NFL',
+              data: []
+            },
+            {
+              name: 'NBA',
+              data: []
+            },
+            {
+              name: 'UFC',
+              data: []
+            },
+            {
+              name: 'Boxing',
+              data: []
+            },
+            {
+              name: 'Wrestling',
+              data: []
+            }
+          ]
+        }
+      ],
+      currentSourceID: null
+    }
+  };
+
+  changeCurrentSource(source=null) {
+    this.setState({
+      currentSourceID: source
+    });
+  }
+
+  componentDidMount() {
+    this.callAPI()
+      .then(res => console.log(res))
+      .catch(err => console.log(err));
+  }
+
+  callAPI = async () => {
+    const response = await axios.get('/api/hello');
+    const body = await response;
+
+    if (response.status !== 200) throw Error(body.message);
+
+    return body;
+  }
+
+  render() {
+    return (
+      <BrowserRouter>
+        <div className="app">
+          <NavBar currentSourceID={this.state.currentSourceID}/>
+          <Route
+            exact path="/"
+            render={() => (
+              <HomeContainer
+                sources={this.state.sources}
+                changeCurrentSource={this.changeCurrentSource}
+              />
+            )}
+          />
+          {
+            this.state.sources.map((source) => {
+              return(
+                <Route
+                  key={source.id}
+                  exact path={`/${source.id}`}
+                  render={(props) => (
+                    <SourcePageContainer
+                      source={source}
+                      path={props.match.path}
+                      changeCurrentSource={this.changeCurrentSource}
+                    />
+                  )}
+                />
+              )
+            })
+          }
+        </div>
+      </BrowserRouter>
+    );
+  };
+}
+
+export default App;
