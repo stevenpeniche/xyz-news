8109c1d455f82010ac6f6407cd7ccd922a698cc9
Steven Peniche <stevenmpeniche@gmail.com>
2018-02-21 16:59:45 -0500

    Add 'News API'

--- server/server.js
+++ server/server.js
@@ -1,14 +1,22 @@
 import express from 'express';
 require('dotenv').config();
+import NewsAPI from 'newsapi';
 
 const app = express();
 const port = process.env.PORT;
 
+const newsapi = new NewsAPI(process.env.NEWS_API_KEY);
+
 app.get('/newsapi', (req, res) => {
-  if(req.query) {
-    res.status(200).send(req.query.source);
+  if(Object.keys(req.query).length !== 0 && req.query.source) {
+    newsapi.v2.everything({
+      sources: req.query.source,
+      language: 'en'
+    }).then((response) => {
+      res.status(200).send(response.articles);
+    });
   } else {
-    res.status(400).send(Error("Take a look at the API Query") );
+    res.status(400).send();
   }
 });
 
