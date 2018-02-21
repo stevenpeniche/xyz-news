83760e791b61dcba760f0d6756998063fef3836d
Steven Peniche <stevenmpeniche@gmail.com>
2018-02-21 14:57:05 -0500

    Switch to Babel Core + Babel Watch

--- server/server.js
+++ server/server.js
@@ -4,8 +4,12 @@ require('dotenv').config();
 const app = express();
 const port = process.env.PORT;
 
-app.get('/api/hello', (req, res) => {
-  res.send({ express: 'Hello From Express' });
+app.get('/newsapi', (req, res) => {
+  if(req.query) {
+    res.status(200).send(req.query.source);
+  } else {
+    res.status(400).send(Error("Take a look at the API Query") );
+  }
 });
 
 app.listen(port, () => console.log(`Server listening on port ${port}`));
