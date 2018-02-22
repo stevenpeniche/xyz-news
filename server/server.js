a7c0fa29c178aaadae01d274bc865f194f5a94a2
Steven Peniche <stevenmpeniche@gmail.com>
2018-02-22 12:28:26 -0500

    Add 'Keyword' Querying of News API

--- server/server.js
+++ server/server.js
@@ -8,13 +8,28 @@ const port = process.env.PORT;
 const newsapi = new NewsAPI(process.env.NEWS_API_KEY);
 
 app.get('/newsapi', (req, res) => {
-  if(Object.keys(req.query).length !== 0 && req.query.source) {
-    newsapi.v2.everything({
-      sources: req.query.source,
-      language: 'en'
-    }).then((response) => {
-      res.status(200).send(response.articles);
-    });
+  if(Object.keys(req.query).length !== 0) {
+    const source = req.query.source;
+    const keyword = req.query.q;
+
+    if(source && !keyword) {
+      newsapi.v2.everything({
+        sources: source,
+        language: 'en'
+      }).then((response) => {
+        res.status(200).send(response.articles);
+      });
+    } else if(source && keyword) {
+      newsapi.v2.everything({
+        sources: source,
+        q: keyword,
+        sortBy: 'publishedAt',
+        language: 'en'
+      }).then((response) => {
+        res.status(200).send(response.articles);
+      });
+    }
+
   } else {
     res.status(400).send();
   }
