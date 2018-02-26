93ae9abdb5959eb5ee259449648ad37a29d871ac
Steven Peniche <stevenmpeniche@gmail.com>
2018-02-26 13:41:34 -0500

    Switch 'Custom Masonry' to 'React Masonry'

--- client/src/components/NewsFeed.js
+++ client/src/components/NewsFeed.js
@@ -1,44 +1,45 @@
 import React from 'react';
+import Masonry from 'react-responsive-masonry';
 import shortid from 'shortid';
 import './NewsFeed.css';
 
 const NewsFeed = (props) => {
   return (
     <div className="news-feed-container">
-      <div className="news-feed-cards-container">
-      {
-        props.topicData.map((article) => {
-          return (
-            <a className="news-feed-card-link" key={shortid.generate()} href={article.url} target="_blank">
-              <div className="news-feed-card levitate animated fadeIn" >
-                {
-                  article.urlToImage !== null?
-                  <div className="news-feed-card-img-container animated fadeIn">
-                    <img
-                      className="news-feed-card-img"
-                      src={article.urlToImage}
-                      alt="news"
-                      style={article.source.id === "bleacher-report"? {maxWidth: "280px"}:{} }
-                    />
+      <Masonry>
+        {
+          props.topicData.map((article) => {
+            return (
+              <a className="news-feed-card-link" key={shortid.generate()} href={article.url} target="_blank">
+                <div className="news-feed-card levitate animated fadeIn" >
+                  {
+                    article.urlToImage !== null?
+                    <div className="news-feed-card-img-container animated fadeIn">
+                      <img
+                        className="news-feed-card-img"
+                        src={article.urlToImage}
+                        alt="news"
+                        style={article.source.id === "bleacher-report"? {maxWidth: "280px"}:{} }
+                      />
+                    </div>
+                    :
+                    <div></div>
+                  }
+                  <div className="news-feed-card-title">
+                    {article.title}
                   </div>
-                  :
-                  <div></div>
-                }
-                <div className="news-feed-card-title">
-                  {article.title}
+                  <div className="news-feed-card-author">
+                    {article.author ? `by ${article.author}` : ""}
+                  </div>
+                  <div className="news-feed-card-description">
+                    {article.description ? article.description : ""}
+                  </div>
                 </div>
-                <div className="news-feed-card-author">
-                  {article.author ? `by ${article.author}` : ""}
-                </div>
-                <div className="news-feed-card-description">
-                  {article.description ? article.description : ""}
-                </div>
-              </div>
-            </a>
-          )
-        })
-      }
-      </div>
+              </a>
+            )
+          })
+        }
+      </Masonry>
     </div>
   );
 }
