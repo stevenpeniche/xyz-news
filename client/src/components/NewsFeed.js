5c9b0d87b8034346f8f5d61e5fd61ed5849137e9
Steven Peniche <stevenmpeniche@gmail.com>
2018-03-01 12:26:27 -0500

    Add Mobile Responsiveness to 'Source Page' & 'News Feed'

--- client/src/components/NewsFeed.js
+++ client/src/components/NewsFeed.js
@@ -1,45 +1,48 @@
 import React from 'react';
-import Masonry from 'react-responsive-masonry';
+import Masonry, {ResponsiveMasonry} from 'react-responsive-masonry';
 import shortid from 'shortid';
 import './NewsFeed.css';
 
 const NewsFeed = (props) => {
   return (
     <div className="news-feed-container">
-      <Masonry>
-        {
-          props.topicData.map((article) => {
-            return (
-              <a className="news-feed-card-link" key={shortid.generate()} href={article.url} target="_blank">
-                <div className="news-feed-card levitate animated fadeIn" >
-                  {
-                    article.urlToImage !== null?
-                    <div className="news-feed-card-img-container animated fadeIn">
-                      <img
-                        className="news-feed-card-img"
-                        src={article.urlToImage}
-                        alt="news"
-                        style={article.source.id === "bleacher-report"? {maxWidth: "280px"}:{} }
-                      />
+      <ResponsiveMasonry
+        columnsCountBreakPoints={{350: 1, 600: 2, 900: 3}}>
+        <Masonry columnsCount={3}>
+          {
+            props.topicData.map((article) => {
+              return (
+                <a className="news-feed-card-link" key={shortid.generate()} href={article.url} target="_blank">
+                  <div className="news-feed-card levitate animated fadeIn" >
+                    {
+                      article.urlToImage !== null?
+                      <div className="news-feed-card-img-container animated fadeIn">
+                        <img
+                          className="news-feed-card-img"
+                          src={article.urlToImage}
+                          alt="news"
+                          style={article.source.id === "bleacher-report"? {maxWidth: "280px"}:{} }
+                        />
+                      </div>
+                      :
+                      <div></div>
+                    }
+                    <div className="news-feed-card-title">
+                      {article.title}
                     </div>
-                    :
-                    <div></div>
-                  }
-                  <div className="news-feed-card-title">
-                    {article.title}
+                    <div className="news-feed-card-author">
+                      {article.author ? `by ${article.author}` : ""}
+                    </div>
+                    <div className="news-feed-card-description">
+                      {article.description ? article.description : ""}
+                    </div>
                   </div>
-                  <div className="news-feed-card-author">
-                    {article.author ? `by ${article.author}` : ""}
-                  </div>
-                  <div className="news-feed-card-description">
-                    {article.description ? article.description : ""}
-                  </div>
-                </div>
-              </a>
-            )
-          })
-        }
-      </Masonry>
+                </a>
+              )
+            })
+          }
+        </Masonry>
+      </ResponsiveMasonry>
     </div>
   );
 }
